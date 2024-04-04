dadfe22e7b4cd622ab7e9112c5ab0e8028352657
Darpan Bhattacharya <dbhattacharya170803@gmail.com>
2024-04-04 18:21:14 +0530

    1 hr time taken, straight lines extracted

--- check_straight_lines.py
+++ check_straight_lines.py
@@ -10,15 +10,47 @@ def display_image(curimg, imgname):
 
 
 cropped_img = cv.imread('cropped.png',0)
+ret, bin_image = cv.threshold(cropped_img, 155, 255, cv.THRESH_BINARY)
 
-mask = np.zeros((5,5), np.uint8)
-mask[:, :] = 255
-mask[1:3, 0:5] = 0
-cv.imwrite("white_mask.jpg",mask) 
+working_image = cv.bitwise_not(bin_image)
+# display_image(cropped_img, 'cropped_img')
+# display_image(bin_image, 'bin image')
+display_image(working_image, 'working_image')
+
+''' PROBABLY THE MASK SIZE AND IMAGE SIZE NEEDS TO BE SAME '''
+
+# mask = np.zeros((5,15), np.uint8)
+mask = np.zeros(working_image.shape[:2], dtype=np.uint8)
+mask[:, :] = 0
+# mask[1:3, 0:15] = 255 ### ei figure ta loop er moddhe modify korte jete jobe, aager line tao include korte hobe
+
+# mask = mask.astype(np.uint8)
+# cv.imwrite("white_mask.jpg", mask) 
+cv.imwrite("black_mask.png", mask)
 # masked_img = cv.bitwise_and(cropped_img, cropped_img, mask=mask)
 
-display_image(mask, 'mask')
+# display_image(mask, 'mask')
 # display_image(masked_img, 'masked image')
 
+# working_img = np.array(bin_image)
 
+# print("mask shape", mask.shape, "working image shape", working_img.shape)
 
+# display_image(working_img, 'working img')
+
+working_image2 = np.array(working_image)
+
+for i in range(80,len(working_image)-5,5):
+    for j in range(0,len(working_image)-15,15):
+        mask[i:i+5, j:j+15] = 255
+        masked_img = cv.bitwise_and(working_image, working_image, mask=mask)
+        white_pix_count = np.sum(masked_img==255)
+        if white_pix_count > 50:
+            working_image2[i:i+5, j:j+15] = 255
+        else:
+            working_image2[i:i+5, j:j+15] = 0
+            # print("i ", i, "j ", j, "white pix count = ", white_pix_count)
+        mask[i:i+5, j:j+15] = 0
+
+cv.imwrite("straight_lines_img.png", working_image2)
+        
